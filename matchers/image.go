6864ec26299c73aae8960e978838719a01473c6b
Erik Sørgaard <erik@jottacloud.com>
2018-11-12 14:32:33 -0500

    jpeg2000 support

--- matchers/image.go
+++ matchers/image.go
@@ -1,29 +1,31 @@
 package matchers
 
 var (
-	TypeJpeg = newType("jpg", "image/jpeg")
-	TypePng  = newType("png", "image/png")
-	TypeGif  = newType("gif", "image/gif")
-	TypeWebp = newType("webp", "image/webp")
-	TypeCR2  = newType("cr2", "image/x-canon-cr2")
-	TypeTiff = newType("tif", "image/tiff")
-	TypeBmp  = newType("bmp", "image/bmp")
-	TypeJxr  = newType("jxr", "image/vnd.ms-photo")
-	TypePsd  = newType("psd", "image/vnd.adobe.photoshop")
-	TypeIco  = newType("ico", "image/x-icon")
+	TypeJpeg     = newType("jpg", "image/jpeg")
+	TypeJpeg2000 = newType("jp2", "image/jpegjp2")
+	TypePng      = newType("png", "image/png")
+	TypeGif      = newType("gif", "image/gif")
+	TypeWebp     = newType("webp", "image/webp")
+	TypeCR2      = newType("cr2", "image/x-canon-cr2")
+	TypeTiff     = newType("tif", "image/tiff")
+	TypeBmp      = newType("bmp", "image/bmp")
+	TypeJxr      = newType("jxr", "image/vnd.ms-photo")
+	TypePsd      = newType("psd", "image/vnd.adobe.photoshop")
+	TypeIco      = newType("ico", "image/x-icon")
 )
 
 var Image = Map{
-	TypeJpeg: Jpeg,
-	TypePng:  Png,
-	TypeGif:  Gif,
-	TypeWebp: Webp,
-	TypeCR2:  CR2,
-	TypeTiff: Tiff,
-	TypeBmp:  Bmp,
-	TypeJxr:  Jxr,
-	TypePsd:  Psd,
-	TypeIco:  Ico,
+	TypeJpeg:     Jpeg,
+	TypeJpeg2000: Jpeg2000,
+	TypePng:      Png,
+	TypeGif:      Gif,
+	TypeWebp:     Webp,
+	TypeCR2:      CR2,
+	TypeTiff:     Tiff,
+	TypeBmp:      Bmp,
+	TypeJxr:      Jxr,
+	TypePsd:      Psd,
+	TypeIco:      Ico,
 }
 
 func Jpeg(buf []byte) bool {
@@ -31,6 +33,23 @@ func Jpeg(buf []byte) bool {
 		buf[0] == 0xFF &&
 		buf[1] == 0xD8 &&
 		buf[2] == 0xFF
+}
+
+func Jpeg2000(buf []byte) bool {
+	return len(buf) > 12 &&
+		buf[0] == 0x0 &&
+		buf[1] == 0x0 &&
+		buf[2] == 0x0 &&
+		buf[3] == 0xC &&
+		buf[4] == 0x6A &&
+		buf[5] == 0x50 &&
+		buf[6] == 0x20 &&
+		buf[7] == 0x20 &&
+		buf[8] == 0xD &&
+		buf[9] == 0xA &&
+		buf[10] == 0x87 &&
+		buf[11] == 0xA &&
+		buf[12] == 0x0
 }
 
 func Png(buf []byte) bool {
