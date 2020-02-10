ac71927eb2e2de59bae1707b31c04a8dbc36b249
Tom <tomas@aparicio.me>
2020-02-10 15:19:56 +0100

    fix(tiff): check min length

--- matchers/image.go
+++ matchers/image.go
@@ -83,7 +83,7 @@ func CR2(buf []byte) bool {
 }
 
 func Tiff(buf []byte) bool {
-	return len(buf) > 3 &&
+	return len(buf) > 9 &&
 		((buf[0] == 0x49 && buf[1] == 0x49 && buf[2] == 0x2A && buf[3] == 0x0) ||
 			(buf[0] == 0x4D && buf[1] == 0x4D && buf[2] == 0x0 && buf[3] == 0x2A)) &&
 		// Differentiate Tiff from CR2
