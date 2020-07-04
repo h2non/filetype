fb8a21f5283547feb024901ae31cc7ee03fcb6eb
sshota0809 <8736380+sshota0809@users.noreply.github.com>
2020-07-04 23:25:17 +0900

    delete unnecessary comment from matchers/text.go

--- matchers/text.go
+++ matchers/text.go
@@ -8,10 +8,7 @@ var Text = Map{
 	TypeHtml: Html,
 }
 
-// Wasm detects a Web Assembly 1.0 filetype.
 func Html(buf []byte) bool {
-	// WASM has starts with `\0asm`, followed by the version.
-	// http://webassembly.github.io/spec/core/binary/modules.html#binary-magic
 	return len(buf) >= 14 &&
 		buf[0] == 0x3C && buf[1] == 0x21 &&
 		buf[2] == 0x64 && buf[3] == 0x6F &&
